06b3aff15e786cb0700c0ea52eaaa3d40efacd41
vattuayushi <vattuayushi@gmail.com>
2025-07-26 00:21:29 +0530

    crud operations

--- src/test/java/org/example/RestAssuredPractice.java
+++ src/test/java/org/example/RestAssuredPractice.java
@@ -1,6 +1,8 @@
 package org.example;
 
 import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.*;
@@ -9,36 +11,71 @@ public class RestAssuredPractice {
 
 
     @Test
-    public void m1(){
-        given().
+    public void getRequest(){
+       Response res1 =  given().
                 when().
-                get("https://reqres.in/api/users?page=2")
-                .then()
-                .statusCode(200);
+                get("https://reqres.in/api/users?page=2");
+       int expectedresponse = 200;
+       int actualResponse = res1.statusCode();
+        System.out.println("Status code of 1st get method is : " + actualResponse);
+        //assert expectedresponse == actualResponse;
+        Assert.assertEquals(actualResponse, expectedresponse);
     }
 
     @Test(description = "Creating New Resource")
-    public void m2(){
+    public void postRequest(){
 
         String payload = "   {\n" +
                 "           \"name\": \"morpheus\",\n" +
                 "            \"job\": \"leader\"\n" +
                 "    }\n";
 
-
         given().
                 contentType(ContentType.JSON)
+                .header("x-api-key", "reqres-free-v1")
                 .body(payload)
                 .when()
-                .get("https://reqres.in/api/users")
+                    .post("https://reqres.in/api/users")
                 .then()
-                .statusCode(201);
+                    .statusCode(201);
+
+    }
+    @Test(description = "Updating the user details")
+    public void putRequest(){
+        String updatePayload =  " {\n" +
+                "           \"name\": \"Salman\",\n" +
+                "            \"job\": \"Khan\"\n" +
+                "    }\n";
+
+       Response newresponse = given()
+                .contentType(ContentType.JSON)
+                .header("x-api-key", "reqres-free-v1")
+                .body(updatePayload)
+                .when()
+                    .put("https://reqres.in/api/users/2");
+        String updatedresponse = newresponse.asPrettyString();
+        System.out.println("Details of Updated User:   " + updatedresponse);
+
+        int actualcode = 201;
+        int Updatedcode = newresponse.statusCode();
+        System.out.println("Status Code Of Put Request :  "  +  Updatedcode);
 
 
+        System.out.println(newresponse.jsonPath().getString("updatedAt"));
+
+        String updated_username = newresponse.jsonPath().getString("name");
+        Assert.assertEquals(updated_username, "Shubham");
 
 
+    }
 
-
-
+    @Test(description = "Deleting user")
+        public void delete(){
+        given()
+                    .header("x-api-key", "reqres-free-v1")
+                .when()
+                    .delete("https://reqres.in/api/users/2")
+                .then()
+                    .statusCode(204);
     }
 }
